a64c6384a43e2f75c6f0250a9d4808aefc9ba409
Zenkina Anastasia Romanovna <anastasia.lllll@yandex.ru>
2025-12-16 19:40:00 +0300

    Добавлен экран home

---
+++ app/src/main/java/com/example/shoesshop/data/view/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com.example.shoesshop.data.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+}
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
